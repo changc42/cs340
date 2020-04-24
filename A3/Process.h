ae1fc98571c6e3ddea92d7252eaeb9fb2cf9490c
Caleb Chang <changc42@gmail.com>
2020-04-24 06:21:55 -0400

    A3: competed working version of code. Now must improve readbility

--- A3/Process.h
+++ A3/Process.h
@@ -12,7 +12,12 @@ class Process{
     int wait;
     int priority;
 
+    
+    
     Process(string id, int b, int s, int p);
+    Process(string id);
+    Process();
+    
     void incrementWait();
     void decrementBurst();
     void display();
